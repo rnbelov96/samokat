fe4bd8286e3b2fff47ae97f980e7dec0285f40c4
Belov Roman <rnbelov96@gmail.com>
2021-03-06 06:08:15 +0300

    Настраивает чекбоксы

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './scss/style.scss';
 import './ts/modals';
+import './ts/checkbox';
---
+++ src/ts/checkbox.ts
@@ -0,0 +1,58 @@
+export {};
+
+const checkboxesData = [
+  {
+    id: 1,
+    data: 'Презентация',
+    isChecked: true,
+  },
+  {
+    id: 2,
+    data: 'Финансовая модель',
+    isChecked: false,
+  },
+  {
+    id: 3,
+    data: 'Бизнес-план',
+    isChecked: false,
+  },
+];
+
+const resultTextEl = document.querySelector('.js-checkbox-text') as HTMLSpanElement;
+const textBoxEl = document.querySelector('.docs__text') as HTMLParagraphElement;
+const picturesElList = document.querySelectorAll('.docs__picture');
+const checkboxElList = document.querySelectorAll('.js-checkbox-input');
+
+const getResultString = ():string => {
+  const stringList: string[] = [];
+  checkboxesData.forEach(el => {
+    if (el.isChecked) {
+      stringList.push(el.data);
+    }
+  });
+
+  return stringList.join(', ');
+};
+
+resultTextEl.textContent = getResultString();
+
+checkboxElList.forEach(el => {
+  el.addEventListener('input', e => {
+    const inputEl = e.currentTarget as HTMLInputElement;
+
+    const inputToEdit = checkboxesData.find(input => input.id === Number(inputEl.dataset.id));
+
+    if (inputToEdit) {
+      inputToEdit.isChecked = !inputToEdit.isChecked;
+      picturesElList[inputToEdit?.id - 1].classList.toggle('docs__picture_shadow');
+    }
+
+    const resultString = getResultString();
+    resultTextEl.textContent = getResultString();
+    if (resultString === '') {
+      textBoxEl.classList.add('visually-hidden');
+    } else {
+      textBoxEl.classList.remove('visually-hidden');
+    }
+  });
+});
