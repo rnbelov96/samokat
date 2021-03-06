7bab6a07d3e976c20bfe41d84ad721b919bd6446
Roman Belov <rnbelov96@gmail.com>
2021-03-06 09:53:40 +0300

    Исправляет недочеты

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,9 @@
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -9,10 +11,7 @@ module.exports = merge(common, {
   devtool: false,
 
   optimization: {
-    minimizer: [
-      new CssMinimizerPlugin(),
-      "..."
-    ],
+    minimizer: [new CssMinimizerPlugin(), '...'],
   },
 
   module: {
@@ -33,6 +32,51 @@ module.exports = merge(common, {
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name]-[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+      chunks: ['main'],
+      minify: {
+        collapseWhitespace: false,
+        keepClosingSlash: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+      // favicon: './src/img/icons/favicon.ico'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/thanks.html'),
+      filename: 'thanks.html',
+      chunks: ['thanks'],
+      minify: {
+        collapseWhitespace: false,
+        keepClosingSlash: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+      // favicon: './src/img/icons/favicon.ico'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/error.html'),
+      filename: 'error.html',
+      chunks: ['thanks'],
+      minify: {
+        collapseWhitespace: false,
+        keepClosingSlash: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+      // favicon: './src/img/icons/favicon.ico'
     }),
   ],
 });
